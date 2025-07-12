648df5eb087930db8af027e0426b04ace0d6c2ad
iNikitina <nikitosik.khv@gmail.com>
2025-07-12 21:36:15 +0300

    Edite Contact.jsx add check for time

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,11 +2,28 @@ import {base_url} from "../utils/constants.js";
 import {useEffect, useState} from "react";
 
 const Contact = () => {
+
     const [planets, setPlanets] = useState([]);
     useEffect(() => {
+        const checkStorage = localStorage.getItem('planets');
+        if (checkStorage) {
+            const { time, data } = JSON.parse(checkStorage);
+            if (Date.now() < time) {
+                setPlanets(data);
+                return;
+            }
+            localStorage.removeItem('planets');
+        }
         fetch(`${base_url}/v1/planets`)
             .then(res => res.json())
-            .then(data => setPlanets(data));
+            .then(data => {
+                // const time = Date.now() + 1000 * 60 * 60 * 24 * 30;
+                const time = Date.now() + 1000 * 5;
+                localStorage.setItem('planets', JSON.stringify({ data, time}));
+                setPlanets(data);
+            })
+            .catch(error => console.log(error));
+
     }, []);
     return (
         <div className="container">
@@ -30,7 +47,7 @@ const Contact = () => {
                 <label htmlFor="subject">Subject</label>
                 <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
 
-                <input type="submit" value="Submit"/>
+                <input type="submit" value="Submit" disabled/>
 
             </form>
         </div>
--- src/components/OpeningCrawl.jsx
+++ src/components/OpeningCrawl.jsx
@@ -5,11 +5,17 @@ const OpeningCrawl = () => {
     const [openingCrawl, setOpeningCrawl] = useState();
 
     useEffect(() => {
-        const episode = Math.floor(Math.random() * 6) + 1;
-        fetch(`${base_url}/v1/films/${episode}`)
-            .then(res => res.json())
-            .then(data => setOpeningCrawl(data.opening_crawl));
-        // return () => console.log('Opening crawl was unmounted');
+        const opening_crawl= sessionStorage.getItem("opening_crawl");
+        if(opening_crawl){
+            setOpeningCrawl(opening_crawl);
+        }else{ const episode = Math.floor(Math.random() * 6) + 1;
+            fetch(`${base_url}/v1/films/${episode}`)
+                .then(res => res.json())
+                .then(data => {
+                    setOpeningCrawl(data.opening_crawl);
+                    sessionStorage.setItem("openingCrawl", data.opening_crawl);
+                }); }
+
     }, [])
 
     if (openingCrawl) {
